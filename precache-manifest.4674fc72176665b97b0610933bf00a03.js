19bff62b3d1ebebe7cb22e16ffa2135ec771112a
Alexander Nicholson <alexn400@gmail.com>
2020-07-12 11:48:41 +1200

    Updates

---
+++ precache-manifest.4674fc72176665b97b0610933bf00a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9da7686b3dd77bc4e5b1cb445dc259",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4a9a2d2b9c8b928168",
+    "url": "/static/css/main.68a29620.chunk.css"
+  },
+  {
+    "revision": "7ba0efd30fea79f4cb36",
+    "url": "/static/js/2.b7860610.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b7860610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4a9a2d2b9c8b928168",
+    "url": "/static/js/main.c25f9d1e.chunk.js"
+  },
+  {
+    "revision": "edd175885a80bcc20c55",
+    "url": "/static/js/runtime-main.063d2709.js"
+  }
+]);
